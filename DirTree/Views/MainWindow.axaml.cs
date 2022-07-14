530219681b46cedf4d13bf6f714fb120138885bf
Ian Wiggins <ian.a.wiggins919@gmail.com>
2022-07-14 12:40:51 -0500

    Add project files.

---
+++ DirTree/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DirTree.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
